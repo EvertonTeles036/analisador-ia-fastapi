8499cf2e11ac41508642393433a39f0282ab895a
EvertonTeles036 <espacocountryvip@gmail.com>
2025-04-06 22:14:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,13 @@ templates = Jinja2Templates(directory="templates")
 async def form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Rota POST que recebe o upload de áudio
+# Rota POST que recebe o upload de 1 áudio
 @app.post("/upload_single")
 async def upload_audio(file: UploadFile = File(...)):
     return {"filename": file.filename}
+
+# Rota POST que recebe múltiplos áudios
+@app.post("/upload_multiple")
+async def upload_audios(files: list[UploadFile] = File(...)):
+    filenames = [file.filename for file in files]
+    return {"filenames": filenames}
