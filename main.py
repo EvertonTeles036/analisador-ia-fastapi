3c840cfc3335f8fea8f79f738288d89cdf66977f
EvertonTeles036 <espacocountryvip@gmail.com>
2025-04-06 22:22:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import List
 
 app = FastAPI()
 
-# Diretório de arquivos estáticos (CSS, imagens etc.)
+# Arquivos estáticos (CSS, imagens, etc)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Diretório de templates HTML
+# Templates HTML
 templates = Jinja2Templates(directory="templates")
 
-# Rota principal que renderiza o formulário
+# Rota principal que renderiza o formulário HTML
 @app.get("/", response_class=HTMLResponse)
-async def form(request: Request):
+async def render_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Rota POST que recebe o upload de 1 áudio
+# Rota para upload de um único áudio
 @app.post("/upload_single")
-async def upload_audio(file: UploadFile = File(...)):
+async def upload_single(file: UploadFile = File(...)):
     return {"filename": file.filename}
 
-# Rota POST que recebe múltiplos áudios
+# Rota para upload múltiplo de áudios
 @app.post("/upload_multiple")
-async def upload_audios(files: list[UploadFile] = File(...)):
+async def upload_multiple(files: List[UploadFile] = File(...)):
     filenames = [file.filename for file in files]
     return {"filenames": filenames}
