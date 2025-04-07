224cf4700202dd6f7fe7796ec005b9e5ac4f65e8
EvertonTeles036 <espacocountryvip@gmail.com>
2025-04-06 21:37:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"mensagem": "API de Análise de Áudio rodando com sucesso"}
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
