6153a61b45a025ba7b84a29d1c5feef5de38a759
EvertonTeles036 <espacocountryvip@gmail.com>
2025-04-06 23:37:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,47 +4,53 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from google.cloud import storage
 import os
-import uuid
+from typing import List
+from datetime import datetime
 
 app = FastAPI()
 
-# Diretório de arquivos estáticos (CSS, imagens etc.)
+# Configuração de arquivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Diretório de templates HTML
+# Templates
 templates = Jinja2Templates(directory="templates")
 
-# Nome do bucket do Google Cloud Storage
-BUCKET_NAME = "audios-atendimentos-minhaempresa"  # Altere para o nome exato do seu bucket
+# Nome do bucket no Google Cloud Storage
+BUCKET_NAME = "audios-atendimentos-minhaempresa"
 
-# Página principal com o formulário
+# Inicializar cliente do Google Cloud Storage
+storage_client = storage.Client()
+
+# Página principal
 @app.get("/", response_class=HTMLResponse)
 async def form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Rota para upload individual
+# Rota para upload de 1 arquivo (individual)
 @app.post("/upload_single")
-async def upload_single(file: UploadFile = File(...)):
-    filename = f"{uuid.uuid4()}_{file.filename}"
-
-    client = storage.Client()
-    bucket = client.bucket(BUCKET_NAME)
-    blob = bucket.blob(filename)
+async def upload_single_file(file: UploadFile = File(...)):
+    # Definindo nome do arquivo com timestamp para evitar conflitos
+    blob_name = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}-{file.filename}"
+    # Escolhendo o bucket do Cloud Storage
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(blob_name)
+    # Realizando o upload do arquivo
     blob.upload_from_file(file.file, content_type=file.content_type)
+    return {"mensagem": f"Arquivo {blob_name} enviado com sucesso."}
 
-    return {"mensagem": "Arquivo enviado com sucesso", "filename": filename}
-
-# Rota para upload múltiplo
+# Rota para upload de múltiplos arquivos
 @app.post("/upload_multiple")
-async def upload_multiple(files: list[UploadFile] = File(...)):
-    client = storage.Client()
-    bucket = client.bucket(BUCKET_NAME)
-
+async def upload_multiple_files(files: List[UploadFile] = File(...)):
     filenames = []
-    for file in files:
-        filename = f"{uuid.uuid4()}_{file.filename}"
-        blob = bucket.blob(filename)
-        blob.upload_from_file(file.file, content_type=file.content_type)
-        filenames.append(filename)
+    # Escolhendo o bucket do Cloud Storage
+    bucket = storage_client.bucket(BUCKET_NAME)
 
-    return {"mensagem": "Arquivos enviados com sucesso", "filenames": filenames}
+    for file in files:
+        # Definindo nome do arquivo com timestamp para evitar conflitos
+        blob_name = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}-{file.filename}"
+        # Realizando o upload do arquivo
+        blob = bucket.blob(blob_name)
+        blob.upload_from_file(file.file, content_type=file.content_type)
+        filenames.append(blob_name)
+
+    return {"mensagem": "Arquivos enviados com sucesso.", "arquivos": filenames}
